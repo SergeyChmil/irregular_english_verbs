ccd995aadb9287a78b904eea9796e34f1496520f
tchmil <sergeytchmil@gmail.com>
2016-12-21 23:59:46 +0200

    added sketch

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: "",
-  styleUrls: ""
+  templateUrl: "./app/html/app.component.html",
+  styleUrls: ["./bower_components/bootstrap/dist/css/bootstrap.min.css", "bower_components/bootstrap/dist/css/bootstrap-theme.min.css", "./app/css/app.component.css"]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title: string = "IRREGULAR ENGLISH VERBS";
+
+  track(string: string) {
+    console.log(string);
+  }
+}
