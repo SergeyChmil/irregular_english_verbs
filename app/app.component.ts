dd5cf83840eaca98a7e726223ad28207f68b3c6a
tchmil <sergeytchmil@gmail.com>
2016-12-21 20:52:58 +0200

    added bower.json

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: "",
+  styleUrls: ""
 })
 export class AppComponent  { name = 'Angular'; }
